cf3bbf6e640e3f91b241724ec8c2135c0e551774
RobertoGuzman64 <robertoguzman64@gmail.com>
2024-02-29 15:34:50 +0100

    seccion 5

--- prueba/app/layout.jsx
+++ prueba/app/layout.jsx
@@ -1,4 +1,5 @@
-import Navbar from "@/components/navbar"
+import Navbar from "@/components/navbar";
+import { Roboto } from "next/font/google";
 
 export const metadata = {
     title: 'Equipzilla',
@@ -6,13 +7,19 @@ export const metadata = {
     icon: 'https://equipzilla.com/favicons/equipzilla/favicon-16x16.png'
 }
 
+const roboto = Roboto({
+    weight:["300","400","500","700"],
+    styles:["italic","normal"],
+    subsets:["latin"]
+})
+
 export default function RootLayout({ children }) {
     return (
         <html>
             <head>
                 <link rel="icon" type="image/png" sizes="16x16" href={metadata.icon} />
             </head>
-            <body>
+            <body className={roboto.className}>
                 <Navbar></Navbar>
                 {children}
             </body>

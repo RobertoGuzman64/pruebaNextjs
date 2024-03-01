0924d142050d4b969a2d2470a1dfbf18e722767a
RobertoGuzman64 <robertoguzman64@gmail.com>
2024-03-01 10:07:50 +0100

    seccion 7

--- prueba/app/page.jsx
+++ prueba/app/page.jsx
@@ -1,3 +1,10 @@
+"use client"
+
 export default function HomePage() {
-    return <div><h1>Home Equipzilla</h1></div>
+    return <section>
+        <h1>Home Equipzilla</h1>
+        <button onClick={() => {
+            alert('Hola')
+        }}>Click</button>
+    </section>
 }
